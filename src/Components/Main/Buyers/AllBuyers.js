8247dc84a23d3b72dd00489022642e3960b8ba97
OvchinnikovJMA <DegraDanko@gmail.com>
2020-08-26 12:07:47 +0500

    V. 1.0.1
    Пагинация работает как нужно

--- src/Components/Main/Buyers/AllBuyers.js
+++ src/Components/Main/Buyers/AllBuyers.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import BuyerRow from './BuyerRow'
 const AllBuyers = (props) => {
     const [Name, SetName] = useState('');
@@ -11,7 +11,7 @@ const AllBuyers = (props) => {
         {
             SetBuyers(props.data);
         }
-        else SetBuyers(Array.from(props.data).filter(elem => elem.name.startsWith(event.target.value)))
+        else SetBuyers(Array.from(Buyers).filter(elem => elem.name.startsWith(event.target.value)))
     }
     const TableClick = (event) => {
         event.target.className === "AverageCheck" && SetBuyers(
@@ -21,60 +21,25 @@ const AllBuyers = (props) => {
         event.target.className === "TotalProfit" && SetBuyers(
             Array.from(Buyers).sort((user1, user2) => user1.TotalProfit > user2.TotalProfit ? 1 : -1))
     }
+    useEffect(() => {
+        SetBuyers(Array.from(props.data).slice(pagination.page*pagination.count - pagination.count, pagination.page * pagination.count))
+    }, [pagination])
+
     const GetPrevPage = () => {
-        Setpagination(pagination.page - 1 === 0 ? {page: 1, count: pagination.count} : {page: pagination.page - 1, count: pagination.count});
-        SetBuyers(
-            ((pagination.count === 5) && Array.from(props.data).slice(pagination.page*5 - 5, pagination.page * 5)) ||
-            ((pagination.count === 10) && Array.from(props.data).slice(pagination.page*10 - 10, pagination.page * 10)) ||
-            ((pagination.count === 15) && Array.from(props.data).slice(pagination.page*15 - 15, pagination.page * 15)))
+        !((pagination.page - 1) === 0) && Setpagination({page: pagination.page - 1, count: pagination.count});        
     }
     const SetFivePagination = () => {    
-        if(pagination.count === 5){
-            Setpagination({page: pagination.page, count: props.data.lenght});
-            SetBuyers(props.data);
-        }   
-        else {
-            SetBuyers(
-                Array.from(props.data).slice(pagination.page*5 - 5, pagination.page * 5));
-                Setpagination({page: pagination.page, count: 5});
-        }
-    }
+        (pagination.count === 5) ? Setpagination({page: pagination.page, count: props.data.length}) : Setpagination({page: pagination.page, count: 5});
+    }   
     const SetTenPagination = () => {
-        if(pagination.count === 10){
-            Setpagination({page: pagination.page, count: props.data.lenght});
-            SetBuyers(props.data);
-        }
-        else{
-            SetBuyers(
-                Array.from(props.data).slice(pagination.page*10 - 10, pagination.page * 10));
-                Setpagination({page: pagination.page, count: 10});
-        }  
-    }  
+        (pagination.count === 10) ? Setpagination({page: pagination.page, count: props.data.length}) : Setpagination({page: pagination.page, count: 10});
+    } 
     const SetFifteenPagination = () => {
-        if(pagination.count === 15){
-            Setpagination({page: pagination.page, count: props.data.lenght});
-            SetBuyers(props.data);
-        }
-        else{
-            SetBuyers(
-                Array.from(props.data).slice(pagination.page*15 - 15, pagination.page * 15));
-                Setpagination({page: pagination.page, count: 15});
-        }  
+        (pagination.count === 15) ? Setpagination({page: pagination.page, count: props.data.length}) : Setpagination({page: pagination.page, count: 15});  
     }
     const GetNextPage = () => {
-        Setpagination(pagination.page * 15 > props.data.lenght ? {page: pagination.page, count: pagination.count} : {page: pagination.page + 1, count: pagination.count});
-        SetBuyers(((pagination.count === 5) && Array.from(props.data).slice(pagination.page*5 - 5, pagination.page * 5)) ||
-        ((pagination.count === 10) && Array.from(props.data).slice(pagination.page*10 - 10, pagination.page * 10)) ||
-        ((pagination.count === 15) && Array.from(props.data).slice(pagination.page*15 - 15, props.data.lenght)))
+        !((pagination.count * pagination.page) >= props.data.length) && Setpagination({page: pagination.page + 1, count: pagination.count});
     }
-    // const SetPagination = (event) => {
-            
-    //     event.target.className === "ten" && 
-
-    //     event.target.className === "fifteen" &&    
-    //     event.target.className === "next" && 
-    // }
-
     return(
         <div className="table-responsive">
             <h2>Покупатели</h2>
